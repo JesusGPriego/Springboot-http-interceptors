f35765d17842be76495d7ee1945efdcd25034d1a
JesusGPriego <suleware@gmail.com>
2024-10-10 12:29:35 +0200

    Interceptor implemented

--- src/main/java/com/suleware/springboot/app/interceptor/springboot_interceptor/controllers/AppController.java
+++ src/main/java/com/suleware/springboot/app/interceptor/springboot_interceptor/controllers/AppController.java
@@ -1,10 +1,24 @@
 package com.suleware.springboot.app.interceptor.springboot_interceptor.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/api/main")
+@RestController
+@RequestMapping("/api/foo")
 public class AppController {
+
+    @GetMapping()
+    public ResponseEntity<Map<String, Object>> foo() {
+        Map<String, Object> json = Collections.singletonMap("message", "handler foo from controller");
+
+        return ResponseEntity.status(HttpStatus.OK.value()).body(json);
+    }
 
 }
